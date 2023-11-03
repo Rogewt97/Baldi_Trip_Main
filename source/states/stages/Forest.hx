f185acad31730dd63369349d979369b564d80178
Walten <tonatiuhgonzalez08@gmail.com>
2023-11-03 09:55:15 -0600

    Gcrew!!!!!
    hello

--- source/states/stages/Forest.hx
+++ source/states/stages/Forest.hx
@@ -5,46 +5,64 @@ import objects.Character;
 
 class Forest extends BaseStage
 {
-	// If you're moving your stage from PlayState to a stage file,
-	// you might have to rename some variables if they're missing, for example: camZooming -> game.camZooming
+  var cielo:BGSprite;
+  var nubes:BGSprite;
+  var atras:BGSprite;
+  var fogata:BGSprite;
+  var fogataf:BGSprite;
+  var layer:BGSprite;
+  var bus:BGSprite;
+  var baldi:BGSprite;
 
 	override function create()
 	{
-        //var fogata:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('shared/images/trip/fogata'));
-        //fogata.setGraphicSize(Std.int(fogata.width * 1.1));
-        //fogata.updateHitbox();
-        //fogata.antialiasing = true;
-        //fogata.scrollFactor.set(0.9, 0.9);
-        //fogata.visible = true;
-
-        var cielo:BGSprite = new BGSprite('forest/cielo', -1100, -1100, 0.9, 0.9);
-        cielo.scrollFactor.set(1, 1);
+        cielo = new BGSprite('forest/cielo', -1100, -1100, 0.9, 0.9);
+        cielo.scrollFactor.set( 0.5, 0.5);
         add(cielo);
-        var nubes:BGSprite = new BGSprite('forest/nubes', -1100, -1100, 0.9, 0.9);
-        nubes.scrollFactor.set(0.0, 0.0);
+
+        nubes= new BGSprite('forest/nubes', -1100, -1100, 0.9, 0.9);
+        nubes.scrollFactor.set(0.5, 0.5);
         add(nubes);
-        var atras:BGSprite = new BGSprite('forest/atras', -1100, -1100, 0.9, 0.9);
+        
+        atras= new BGSprite('forest/atras', -1100, -1100, 0.9, 0.9);
         atras.scrollFactor.set(1, 1);
         add(atras);
-        var fogata:BGSprite = new BGSprite('forest/fogata', -1100, -1100, 0.9, 0.9);
+
+        fogata= new BGSprite('forest/fogata', -1100, -1100, 0.9, 0.9);
         fogata.scrollFactor.set(1, 1);
         add(fogata);
 
+        if(!ClientPrefs.data.lowQuality)
+          {
+            fogataf = new BGSprite('forest/fogataf', 500, 300, 0.9, 0.9, ['fuego_fogata']);
+            fogataf.scrollFactor.set(1, 1);
+            add(fogataf);
+          }
 	}
-	
+
 	override function createPost()
 	{
-         var bus:BGSprite = new BGSprite('forest/bus', -1100, -1100, 0.9, 0.9);
-         bus.scrollFactor.set(1, 1);
-         add(bus);
-      /*  var bus:FlxSprite = new FlxSprite(0, -200).loadGraphic(Paths.image('forest/bus'));
-        bus.setGraphicSize(Std.int(bus.width * 1.1));
-        bus.updateHitbox();
-        bus.antialiasing = true;
-        bus.scrollFactor.set(0.9, 0.9);
-        bus.visible = true;
-        add(bus);*/
+      layer = new BGSprite('forest/layer', -1000, -1100, 0.9, 0.9);
+      //bus.scrollFactor.set(1, 1);
+      add(layer);
 
-    }
+      bus = new BGSprite('forest/bus', -1100, -1100, 0.9, 0.9);
+      //bus.scrollFactor.set(1, 1);
+      add(bus);
 
+      //baldi = new BGSprite('forest/baldi', -800, -400, 0, 0 );
+      //baldi.alpha = 0;
+		  //baldi.blend = ADD;
+      //add(baldi);
+  }
+  override function countdownTick(count:Countdown, num:Int) if(num % 2 == 0) everyoneDance();
+	override function beatHit() everyoneDance();
+	function everyoneDance()
+	{
+		if(!ClientPrefs.data.lowQuality) fogataf.dance();
+		/*foregroundSprites.forEach(function(spr:BGSprite)
+		{
+			spr.dance();
+		});*/
+	}
 }
